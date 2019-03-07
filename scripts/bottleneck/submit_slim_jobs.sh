efd871526ecd4acb4c701b36eadf6338f84baee1
Chris Kyriazis <ckyriazis33@gmail.com>
2019-03-06 16:01:56 -0800

    initial commit

---
+++ scripts/bottleneck/submit_slim_jobs.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck
+#$ -l h_rt=32:00:00,h_data=32G,highp
+#$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
+#$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
+#$ -N bottleneck_10000Na_25Nb_h0.0_3619
+#$ -m bea
+#$ -t 1:10
+
+# MAKE SURE TO CHANGE NAME OF JOB
+
+source /u/local/Modules/default/init/modules.sh
+module load gcc/4.9.3
+
+SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
+
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_10000Na_25Nb_h0.0_3619.slim
+
