b0a1380f58cb25c6a510b22e833e429970f54818
ckyriazis <ckyriazis33@gmail.com>
2019-03-06 16:13:28 -0800

    initial commit

--- scripts/bottleneck/submit_slim_jobs.sh
+++ scripts/bottleneck/submit_slim_jobs.sh
@@ -3,7 +3,7 @@
 #$ -l h_rt=32:00:00,h_data=32G,highp
 #$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
 #$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
-#$ -N bottleneck_10000Na_25Nb_h0.0_3619
+#$ -N bottleneck_1000Na_50Nb_h0.0_3619
 #$ -m bea
 #$ -t 1:10
 
@@ -14,5 +14,5 @@ module load gcc/4.9.3
 
 SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
 
-${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_10000Na_25Nb_h0.0_3619.slim
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_1000Na_50Nb_h0.0_3619.slim
 
