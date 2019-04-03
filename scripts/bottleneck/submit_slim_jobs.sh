013a42bc23213cac5443e05caf66c154c5714a55
ckyriazis <ckyriazis33@gmail.com>
2019-04-03 11:27:53 -0700

    added function to bottleneck scripts

--- scripts/bottleneck/submit_slim_jobs.sh
+++ scripts/bottleneck/submit_slim_jobs.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck
-#$ -l h_rt=200:00:00,h_data=40G,highp
-#$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
-#$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output
-#$ -N bottleneck_10000Na_100Nb_100nF_h0.0_31319
+#$ -l h_rt=312:00:00,h_data=96G,highp
+#$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/bottleneck
+#$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/bottleneck
+#$ -N bottleneck_15000Na_100Nb_100nF_h0.0_4319
 #$ -m bea
-#$ -t 1:25
+#$ -t 1:10
 
 # MAKE SURE TO CHANGE NAME OF JOB
 
@@ -14,4 +14,4 @@ module load gcc/4.9.3
 
 SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
 
-${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_10000Na_100Nb_100nF_h0.0_31319.slim
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_15000Na_100Nb_100nF_h0.0_4319.slim
--- scripts/rescue/submit_slim_jobs.sh
+++ scripts/rescue/submit_slim_jobs.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-#$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck
-#$ -l h_rt=220:00:00,h_data=56G,highp
+#$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue
+#$ -l h_rt=140:00:00,h_data=48G,highp
 #$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/rescue
 #$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/rescue
-#$ -N rescue_1000Ns_1000Ts_25Nb_5nM_25Tr_h0.0_32919
+#$ -N rescue_1000Ns_1000Ts_25Nb_5nM_10Tr_h0.0_4219
 #$ -m bea
 #$ -t 1:10
 
@@ -14,4 +14,4 @@ module load gcc/4.9.3
 
 SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
 
-${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue/slim_rescue_1000Ns_1000Ts_25Nb_5nM_25Tr_h0.0_32919.slim
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue/slim_rescue_1000Ns_1000Ts_25Nb_5nM_10Tr_h0.0_4219.slim
