8eefc4b28b9cb4a8b56a5d3798bdcbdc4485de4c
ckyriazis <ckyriazis33@gmail.com>
2019-12-04 15:49:50 -0800

    new scripts for revisions

--- scripts/bottleneck/submit_slim_jobs.sh
+++ scripts/bottleneck/submit_slim_jobs.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck
-#$ -l h_rt=336:00:00,h_data=96G,highp
+#$ -l h_rt=200:00:00,h_data=48G,highp
 #$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/bottleneck
 #$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/bottleneck
-#$ -N bottleneck_15000Na_25Nb_25nF_h0.0_neutral_het_6519
+#$ -N bottleneck_10000Na_25Nb_h0.0_3619
 #$ -m bea
-#$ -t 1:10
+#$ -t 1:25
 
 # MAKE SURE TO CHANGE NAME OF JOB
 
@@ -14,4 +14,4 @@ module load gcc/4.9.3
 
 SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
 
-${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/slim_bottleneck_15000Na_25Nb_25nF_h0.0_neutral_het_6519.slim  
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/bottleneck/run_scripts/slim_bottleneck_10000Na_25Nb_h0.0_3619.slim 
--- scripts/rescue/submit_slim_jobs.sh
+++ scripts/rescue/submit_slim_jobs.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #$ -wd /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue
-#$ -l h_rt=140:00:00,h_data=48G,highp
+#$ -l h_rt=200:00:00,h_data=48G,highp
 #$ -o /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/rescue
 #$ -e /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/output/rescue
-#$ -N rescue_10000Ns_50Ts_25Nb_5nM_1nR_h0.0_het_5319
+#$ -N rescue_larger_recipient_25Ns_10Ts_50Nb_5nM_1nR_h0.0_112619
 #$ -m bea
-#$ -t 1:25
+#$ -t 1:40
 
 
 # MAKE SURE TO CHANGE NAME OF JOB
@@ -15,4 +15,4 @@ module load gcc/4.9.3
 
 SLIMDIR=/u/home/c/ckyriazi/project-klohmuel/software/slim_build
 
-${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue/slim_rescue_10000Ns_50Ts_25Nb_5nM_1nR_h0.0_het_5319.slim
+${SLIMDIR}/slim /u/home/c/ckyriazi/project-klohmuel/genetic_rescue_simulations/scripts/rescue/slim_rescue_larger_recipient_25Ns_10Ts_50Nb_5nM_1nR_h0.0_112619.slim
